d504f5e43e7ca807dd49033fa269c5cc4a578de7
Liz-Landin <241405735@cecytejalisco.edu.mx>
2025-09-15 16:53:48 -0600

    Delete PYTHON2 directory

--- PYTHON2/PROGRAMA2.py
+++
@@ -1,8 +0,0 @@
-#PROGRAMA2
-#LIZETHELANDINRIOS
-edad=int(input("INGRESE SU EDAD"))
-if (edad>=18):
-    print("LICENCIA OBTENIDA")
-else :
-    print("LICENCIA NO OBTENIDA")
-print("FIN DEL PROGRAMA")
--- PYTHON2/PROGRAMA5.py
+++
@@ -1,9 +0,0 @@
-#PROGRAMA5
-#LIZETHELANDINRIOS
-num=int(input("INGRESA UN  NUMERO"))
-modulo=num%2
-if (modulo==0):
-     print("El numero es par")
-else:
-     print("El numero es impar")
-print("FIN DEL PROGRAMA")
